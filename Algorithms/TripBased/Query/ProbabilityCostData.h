6a347fb04b90e0b6d32fa06e5691b85c868d9833
PatrickSteil <patricksteil@freenet.de>
2026-08-12 15:04:44 +0200

    cleanup

--- Algorithms/TripBased/Query/ProbabilityCostData.h
+++ Algorithms/TripBased/Query/ProbabilityCostData.h
@@ -1,21 +1,30 @@
 #pragma once
+
 #include "../../../DataStructures/TripBased/Data.h"
+
 #include <cassert>
 #include <limits>
 #include <vector>
+
 namespace TripBased {
+
 class ProbabilityCostData {
+
 public:
   static constexpr double Infinity = std::numeric_limits<double>::infinity();
+
   ProbabilityCostData(const Data &data)
       : cost(data.numberOfStopEvents(), Infinity) {}
+
   inline void clear() noexcept {
     std::fill(cost.begin(), cost.end(), Infinity);
   }
+
   inline double operator()(const StopEventId stopEvent) const noexcept {
     assert(stopEvent < cost.size());
     return cost[stopEvent];
   }
+
   inline StopEventId getScanEnd(const StopEventId begin, const StopEventId end,
                                 const double newCost) const noexcept {
     assert(begin >= end || begin < cost.size());
@@ -26,15 +35,12 @@ public:
     }
     return end;
   }
+
   inline void update(const StopEventId stopEvent, const StopEventId tripEnd,
                      const StopEventId routeEnd, const StopIndex tripLength,
                      const double newCost) noexcept {
-    assert(tripEnd <= cost.size() &&
-           "tripEnd exceeds numberOfStopEvents() -- check "
-           "data.firstStopEventOfTrip's sentinel entry");
-    assert(routeEnd <= cost.size() &&
-           "routeEnd exceeds numberOfStopEvents() -- check "
-           "data.firstTripOfRoute's sentinel entry for the last route");
+    assert(tripEnd <= cost.size());
+    assert(routeEnd <= cost.size());
     StopEventId currentStart = stopEvent;
     StopEventId currentEnd = tripEnd;
     for (; currentStart < routeEnd;
